82099ba6a557f61bb76ec70704584d42767203ca
Peter Barbatsis <pbarbats@openup0-42.bravo>
2017-07-03 09:03:39 -0500

    bigger 2nd commit with like everything

---
+++ tests/FirstTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace stats\Test;
+require 'vendor/autoload.php';
+
+
+class FirstTest extends \PHPUnit_Framework_TestCase
+{
+    public function testUselessness()
+    {
+        $this->assertTrue(true);
+    }
+    //
+}
